9f969e82affd74be87e11261b3348198695a7e1a
Sivakumar V <sivakumar.bdu@gmail.com>
2016-05-18 17:46:31 +0530

    example for starting rails server

---
+++ run.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#Start Rails webric server, should be placed inside root of rails application
+run_server() {
+	echo "Trying to run server..."
+	PID_FILE=tmp/pids/server.pid
+	if [ -s $PID_FILE ]
+	then
+		PID=$(cat "$PID_FILE")
+		echo "PID :  $PID"
+		kill -9 $PID
+	fi
+	bundle exec rails server
+}
+
+check_status(){
+	if [ "$1" -ne "0" ]
+	then
+		echo "exit script with status $1"
+		exit $1
+	fi
+}
+
+run_server
+check_status $?
