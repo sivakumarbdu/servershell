747b02265b8f02a426afc27957fabee3c291f1d2
Sivakumar V <sivakumar.bdu@gmail.com>
2016-05-19 19:10:02 +0530

    add script to copy the code to server and run it

--- provisioning/env.sh
+++ provisioning/env.sh
@@ -1,2 +1,6 @@
 export SCRIPT_RUBY_VERSION=1.9.3
-export SCRIPT_MODULES="rvm ruby"
+export SCRIPT_NODE_VERSION=4.4
+export SCRIPT_MODULES="rvm ruby nvm node mysql"
+export PEM_FILE=""
+export USER=""
+export IP=""
--- provisioning/run_server.sh
+++ provisioning/run_server.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+set_environment() {
+	source env.sh
+}
+
+
 run_script_on_server() {
 
 ssh  -i $PEM_FILE ${USER}@${IP} << 'SCRIPT'
@@ -14,6 +19,24 @@ remove_scripts() {
 SCRIPT
 }
 
-source env.sh
+run_command_on_server() {
+
+ssh  -i $PEM_FILE ${USER}@${IP} << 'SCRIPT'
+  cd /tmp/pscript;
+  mkdir /tmp/pscript;
+SCRIPT
+}
+
+copy_to_server(){
+  scp  -i $PEM_FILE  -r * ${USER}@${IP}:/tmp/pscript
+}
+
+
+#copy files
+set_environment
+run_command_on_server mkdir /tmp/pscript
+copy_to_server
+
+#run script
 run_script_on_server
 remove_scripts
