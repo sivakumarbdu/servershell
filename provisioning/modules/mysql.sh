ebee5526067d7292f672be4d8c840177821e60a2
Sivakumar V <sivakumar.bdu@gmail.com>
2016-05-19 23:59:27 +0530

    remove the module installer file and move to folder

--- provisioning/modules/mysql.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-if [ -e "/etc/init.d/mysql" ]
-  then
-    echo "Mysql already installed"
-  else
-  	sudo apt-get -y install mysql-server
-fi
--- provisioning/modules/postgres.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo "Module Postgresql"
-if [ -e "/etc/init.d/postgresql" ]
-  then
-  echo "Postgresql already installed"
-else
-  sudo apt-get -y install  postgresql postgresql-contrib
-fi
