d92a558a20340b20494cb2e0f93d037fe5671426
Sivakumar V <sivakumar.bdu@gmail.com>
2016-05-19 16:24:00 +0530

    reinstall ruby if exists

--- provisioning/modules/ruby.sh
+++ provisioning/modules/ruby.sh
@@ -8,8 +8,11 @@ if [ "$RUBY" -ne "0" ]
   then
   INSTALLED_RUBY_VERSION=$(ruby --version)
   echo "Ruby already installed $INSTALLED_RUBY_VERSION"
-  echo "Installing again ruby $INSTALLED_RUBY_VERSION"
+  echo "Reinstalling ruby $INSTALLED_RUBY_VERSION"
+  rvm reinstall $SCRIPT_RUBY_VERSION
+else
+  rvm install $SCRIPT_RUBY_VERSION
 fi
-rvm install $SCRIPT_RUBY_VERSION
 }
+
 install_ruby
