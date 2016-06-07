140392f00a1e7bac42d23d4f8c2b8c32c78ea4cd
Sivakumar V <sivakumar.bdu@gmail.com>
2016-06-07 18:20:54 +0530

    call module rvm_ruby

--- provisioning/env.sh
+++ provisioning/env.sh
@@ -1,6 +1,3 @@
 export SCRIPT_RUBY_VERSION=1.9.3
 export SCRIPT_NODE_VERSION=4.4
-export SCRIPT_MODULES="rvm ruby nvm node mysql"
-export PEM_FILE=""
-export USER=""
-export IP=""
+export SCRIPT_MODULES="rvm_ruby nvm node mysql"
