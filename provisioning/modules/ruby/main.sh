eed9c44d10dee6795b3a31aef50d27912dc9af58
Sivakumar V <sivakumar.bdu@gmail.com>
2016-06-07 18:17:22 +0530

    fix to install ruby with rvm

--- provisioning/modules/ruby/main.sh
+++ provisioning/modules/ruby/main.sh
@@ -9,9 +9,9 @@ if [ "$RUBY" -ne "0" ]
   INSTALLED_RUBY_VERSION=$(ruby --version)
   echo "Ruby already installed $INSTALLED_RUBY_VERSION"
   echo "Reinstalling ruby $INSTALLED_RUBY_VERSION"
-  rvm reinstall $SCRIPT_RUBY_VERSION
+   ~/.rvm/bin/rvm reinstall $SCRIPT_RUBY_VERSION
 else
-  rvm install $SCRIPT_RUBY_VERSION
+   ~/.rvm/bin/rvm install $SCRIPT_RUBY_VERSION
 fi
 }
 
